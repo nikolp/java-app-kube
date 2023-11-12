c9449a9d8f6a02c47bf3a4acab660c94b3da11ff
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-11-12 17:15:14 -0500

    lint

--- src/test/java/com/example/appkube/MainControllerFullStackTest.java
+++ src/test/java/com/example/appkube/MainControllerFullStackTest.java
@@ -19,7 +19,7 @@ class MainControllerFullStackTest {
     private TestRestTemplate restTemplate;
 
     @Test
-    void greetingShouldReturnDefaultMessage() throws Exception {
+    void greetingShouldReturnDefaultMessage()  {
         assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
                 String.class)).contains("Hello World");
     }
