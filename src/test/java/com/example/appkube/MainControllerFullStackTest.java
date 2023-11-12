59b3210c8ea3cbd1ba609ecad4cfa8fc8cdd7fba
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-11-12 17:13:20 -0500

    example of TestRestTemplate with full server test

---
+++ src/test/java/com/example/appkube/MainControllerFullStackTest.java
@@ -0,0 +1,26 @@
+package com.example.appkube;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+// This test actually starts the whole Tomcat server (but on a random port)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MainControllerFullStackTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void greetingShouldReturnDefaultMessage() throws Exception {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).contains("Hello World");
+    }
+}
