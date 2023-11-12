0d0e2d06834bbf0776bce4986f77c73793aa3303
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-11-12 17:14:01 -0500

    add logging

--- src/main/java/com/example/appkube/MainController.java
+++ src/main/java/com/example/appkube/MainController.java
@@ -1,12 +1,15 @@
 package com.example.appkube;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class MainController {
     @GetMapping("/")
     VersionResponse index() {
+        log.info("index() called");
         return new VersionResponse("Hello World!", 1);
     }
 }
