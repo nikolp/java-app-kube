5dbf5bd495f36688440255d1835c41ffaae60cac
Philip Nikolov <philip.k.nikolov@gmail.com>
2023-11-12 21:00:01 -0500

    Add version.

--- src/main/java/com/example/appkube/MainController.java
+++ src/main/java/com/example/appkube/MainController.java
@@ -1,15 +1,19 @@
 package com.example.appkube;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class MainController {
+    @Value("${version:1}")
+    int version;
+
     @GetMapping("/")
     VersionResponse index() {
         log.info("index() called");
-        return new VersionResponse("Hello World!", 1);
+        return new VersionResponse("Hello World!", version);
     }
 }
